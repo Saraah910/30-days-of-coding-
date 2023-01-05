9d3090eb23cc4ba103222a20ca78d68e64273734
Sakshi Rajesh Aherkar <93570056+Saraah910@users.noreply.github.com>
2023-01-05 23:52:45 +0530

    Create short_conti_sub.py

---
+++ 1st jan 2023/short_conti_sub.py
@@ -0,0 +1,29 @@
+def findUnsortedSubarray(nums):
+    if len(nums) <2:
+        return 0
+    
+    prev = nums[0]
+    end = 0
+	# find the largest index not in place
+    for i in range(0, len(nums)):
+        if nums[i] < prev:
+            end = i
+        else:
+            prev = nums[i]
+
+    start = len(nums) - 1
+    prev = nums[start]
+	# find the smallest index not in place
+    for i in range(len(nums)-1, -1, -1):
+        if prev < nums[i]:
+            start = i
+        else:
+            prev = nums[i]
+    if end != 0:
+        return end - start + 1
+    else: 
+        return 0
+        
+nums = [2,6,4,8,10,9,15]
+res= findUnsortedSubarray(nums)
+print(res)
